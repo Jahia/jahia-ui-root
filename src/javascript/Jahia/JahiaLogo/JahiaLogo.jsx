f793cdee5a370ef3e2d0a0a6bd8be30b0ba99030
alex <alexandrk_@hotmail.com>
2019-11-06 13:59:38 -0500

    BACKLOG-11280 Improve logo component

--- src/javascript/Jahia/JahiaLogo/JahiaLogo.jsx
+++ src/javascript/Jahia/JahiaLogo/JahiaLogo.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Svg from './jahiaLogo.svg';
 
-const JahiaLogo = () => (
-    <Svg style={{width: '6rem', height: '2rem'}} viewBox="0 0 176 80"/>
+const JahiaLogo = ({width, height}) => (
+    <Svg style={{width: width, height: height}} viewBox="0 0 176 80"/>
 );
+
+JahiaLogo.defaultProps = {
+    width: '6rem',
+    height: '2rem'
+};
+
+JahiaLogo.propTypes = {
+    width: PropTypes.string,
+    height: PropTypes.string
+};
 
 export default JahiaLogo;
