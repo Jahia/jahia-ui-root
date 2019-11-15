d38886e764907e4dfece141b80dbad35ee642987
astritademi <aademi@jahia.com>
2019-11-15 12:57:32 -0500

    BACKLOG 11519 Added jest-image-snapshot and jest-teamcity-reporter to tests

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,16 @@
-const presets = [
-    [
-        '@babel/env',
-        {
-            targets: {
-                browsers: ['last 2 versions', '> 5%']
-            },
-            modules: process.env.BABEL_MODULES ?
-                process.env.BABEL_MODULES :
-                'commonjs' // Babel's default is commonjs
-        }
-    ]
-];
+// const presets = [
+//     [
+//         '@babel/env',
+//         {
+//             targets: {
+//                 browsers: ['last 2 versions', '> 5%']
+//             },
+//             modules: process.env.BABEL_MODULES ?
+//                 process.env.BABEL_MODULES :
+//                 'commonjs' // Babel's default is commonjs
+//         }
+//     ]
+// ];
 
 module.exports = {
     sourceMaps: 'inline',
