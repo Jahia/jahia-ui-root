8aa3d675866e192cd24d02bc603ef18299d63b0c
pbraich <pbraich@jahia.com>
2019-12-02 12:24:05 -0500

    BACKLOG-11700 added signout tests

---
+++ src/javascript/__tests__/signout.test.js
@@ -0,0 +1,34 @@
+import tf from './testFunctions';
+
+
+const {toMatchImageSnapshot} = require('jest-image-snapshot');
+expect.extend({toMatchImageSnapshot});
+
+beforeEach(async () => {
+    await page.goto(tf.testURL, {waitUntil: 'domcontentloaded'});
+});
+
+describe('Navigation - test signout', () => {
+
+    test('Sign out exists', async () => {
+
+        await tf.assertElemExistence('nav', true);
+        await tf.clickOnElem('button');
+        await tf.assertVisibilityOfElementByXpath('//div[contains(text(), \'Test environment - en\')]', true);
+
+        await page.$x("//ul/li/div[3]/button").then(element => expect(element[0].screenshot({"encoding":"base64"})).resolves.toMatchImageSnapshot());
+    }, tf.timeout);
+
+    test('logout from jahia', async () => {
+        await tf.assertElemExistence('nav', true);
+        await tf.clickOnElem('button');
+
+        const button = await page.waitForXPath('//button[text()="Sign out"]');
+        await button.click();
+        await page.waitForNavigation({waitUntil: 'load'});
+
+        await tf.assertElemExistence(".login-form", true)
+        //expect(page.$('.login-form')).toHaveLength(1)
+
+    }, tf.timeout);
+});
