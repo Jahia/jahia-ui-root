a12c546804c4f90bc71d2b63f4f350f4db23ecc0
astritademi <aademi@jahia.com>
2019-12-16 11:32:56 -0500

    BACKLOG-11964 consolidated mock files into one dedicated __mocks__ folder, added jest-teamcity-reporter

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,8 @@ module.exports = {
     ],
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-            '<rootDir>/__mocks__/fileMock.js',
-        '\\.svg$': '<rootDir>/__mocks__/svgMock.js'
-    }
+            '<rootDir>/src/javascript/__mocks__/fileMock.js',
+        '\\.svg$': '<rootDir>/src/javascript/__mocks__/svgMock.js'
+    },
+    testResultsProcessor: "jest-teamcity-reporter"
 };
