9c48ec7ab360f52bd3f944a6fc59086928074f1b
alex <alexandrk_@hotmail.com>
2020-01-03 11:10:00 -0500

    BACKLOG-11704 Fix namespace resolution, get rid of locale loading error

--- src/main/java/org/jahia/modules/jahiaui/taglib/TagFunctions.java
+++ src/main/java/org/jahia/modules/jahiaui/taglib/TagFunctions.java
@@ -1,8 +1,10 @@
 package org.jahia.modules.jahiaui.taglib;
 
 import org.apache.commons.lang.StringUtils;
+import org.jahia.data.templates.JahiaTemplatesPackage;
 import org.jahia.osgi.BundleUtils;
 import org.jahia.osgi.FrameworkService;
+import org.jahia.services.templates.JahiaTemplateManagerService;
 import org.osgi.framework.Bundle;
 
 import java.util.Arrays;
@@ -22,17 +24,34 @@ public class TagFunctions {
      * @return a string representation of an array with all i18n namespaces
      */
     public static String getI18nNameSpaces() {
-        // TODO Strengthen once module ecosystem is more mature to include only modules that are supposed to work with ui-root
         Collection<Bundle> bundles = Arrays.stream(FrameworkService.getBundleContext().getBundles())
                 .filter(bundle -> bundle.getState() == Bundle.ACTIVE && BundleUtils.isJahiaModuleBundle(bundle))
                 .collect(Collectors.toList());
         Set<String> namespaces = new LinkedHashSet<>();
+        JahiaTemplateManagerService jtms = BundleUtils.getOsgiService(JahiaTemplateManagerService.class, null);
+
         for (Bundle bundle : bundles) {
-            if (bundle.getEntry("/javascript/locales") != null) {
+            if (dependsOnJahiaUIRoot(jtms, bundle.getHeaders().get("Bundle-Name")) && bundle.getEntry("/javascript/locales") != null) {
                 namespaces.add(BundleUtils.getModuleId(bundle));
             }
         }
 
         return namespaces.isEmpty() ? "[]" : "['" + StringUtils.join(namespaces, "', '") + "']";
+    }
+
+    private static boolean dependsOnJahiaUIRoot(JahiaTemplateManagerService jtms, String bundleName) {
+        if (jtms == null) return false;
+
+        JahiaTemplatesPackage pack = jtms.getTemplatePackage(bundleName);
+
+        if (pack == null) return false;
+
+        for (JahiaTemplatesPackage dependency : pack.getDependencies()) {
+            if (dependency.getId().equals("jahia-ui-root")) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
