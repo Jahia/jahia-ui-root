8cdefebf6cb389df1aa5f0fb573205dd779e788f
Thomas Draier <tdraier@jahia.com>
2020-01-31 15:51:10 +0100

    BACKLOG-12157: Added redux provider (#34)

--- src/javascript/Jahia/Jahia.container.jsx
+++ src/javascript/Jahia/Jahia.container.jsx
@@ -7,42 +7,45 @@ import Jahia from './Jahia';
 import PrimaryNavGroup from './PrimaryNavGroup';
 import JahiaContext from './Jahia.context';
 import './Jahia.routes';
+import {ReduxProvider} from './ReduxProvider';
 
 const JahiaContainer = ({jahiaCtx}) => {
     actions(registry);
 
     return (
-        <BrowserRouter basename={jahiaCtx.contextPath + '/modules/moonstone'}>
-            <JahiaContext.Provider value={jahiaCtx}>
-                <Jahia routes={registry.find({type: 'route', target: 'nav-root-top'})}
-                       topNavGroups={[
-                           <PrimaryNavGroup key="topNavGroup"
-                                            navItems={registry.find({
+        <ReduxProvider jahiaCtx={jahiaCtx}>
+            <BrowserRouter basename={jahiaCtx.contextPath + '/modules/moonstone'}>
+                <JahiaContext.Provider value={jahiaCtx}>
+                    <Jahia routes={registry.find({type: 'route', target: 'nav-root-top'})}
+                           topNavGroups={[
+                               <PrimaryNavGroup key="topNavGroup"
+                                                navItems={registry.find({
                                                     type: 'topNavGroup',
                                                     target: 'nav-root-top'
                                                 })}/>
                            ]}
-                       bottomNavGroups={[
-                           <PrimaryNavGroup key="bottomDocumentationGroup"
-                                            isDisplayedWhenCollapsed={false}
-                                            navItems={registry.find({
+                           bottomNavGroups={[
+                               <PrimaryNavGroup key="bottomDocumentationGroup"
+                                                isDisplayedWhenCollapsed={false}
+                                                navItems={registry.find({
                                                     type: 'bottomDocumentationGroup',
                                                     target: 'nav-root-bottom'
                                                 })}/>,
-                           <PrimaryNavGroup key="bottomProfileGroup"
-                                            navItems={registry.find({
-                                   type: 'bottomProfileGroup',
-                                   target: 'nav-root-bottom'
-                               })}/>,
-                           <PrimaryNavGroup key="bottomAdminGroup"
-                                            navItems={registry.find({
-                                   type: 'bottomAdminGroup',
-                                   target: 'nav-root-bottom'
-                               })}/>
+                               <PrimaryNavGroup key="bottomProfileGroup"
+                                                navItems={registry.find({
+                                                    type: 'bottomProfileGroup',
+                                                    target: 'nav-root-bottom'
+                                                })}/>,
+                               <PrimaryNavGroup key="bottomAdminGroup"
+                                                navItems={registry.find({
+                                                    type: 'bottomAdminGroup',
+                                                    target: 'nav-root-bottom'
+                                                })}/>
                            ]}
                     />
-            </JahiaContext.Provider>
-        </BrowserRouter>
+                </JahiaContext.Provider>
+            </BrowserRouter>
+        </ReduxProvider>
     );
 };
 
