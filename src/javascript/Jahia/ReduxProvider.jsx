70d596204d11535882f45593e874fab3fe82de90
Thomas Draier <tdraier@jahia.com>
2020-02-01 23:34:07 +0100

    BACKLOG-12158 : Add connected router

--- src/javascript/Jahia/ReduxProvider.jsx
+++ src/javascript/Jahia/ReduxProvider.jsx
@@ -2,13 +2,9 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import {registry} from '@jahia/ui-extender';
 import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
-import {batchDispatchMiddleware} from 'redux-batched-actions';
-import thunk from 'redux-thunk';
 import PropTypes from 'prop-types';
 
-export const ReduxProvider = ({jahiaCtx, children}) => {
-    console.log(jahiaCtx);
-    // TODO const history = createBrowserHistory({basename: jahiaCtx.contextPath + jahiaCtx.urlbase});
+export const ReduxProvider = ({children}) => {
     const reducersArray = registry.find({type: 'redux-reducer'});
     const reducerObj = {};
     reducersArray.forEach(r => {
@@ -19,24 +15,22 @@ export const ReduxProvider = ({jahiaCtx, children}) => {
 
     const composeEnhancers = window.top.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-    const enhancer = composeEnhancers(
-        applyMiddleware(
-            // RouterMiddleware(history),
-            batchDispatchMiddleware,
-            thunk
-        )
-    );
+    const middlewares = registry.find({type: 'redux-middleware'}).map(m => m.middleware);
+
+    const enhancer = composeEnhancers(applyMiddleware.apply(this, middlewares));
+    const store = createStore(rootReducer, enhancer);
+
+    const listeners = registry.find({type: 'redux-listener'}).map(l => l.createListener(store));
+    listeners.forEach(l => store.subscribe(l));
 
     return (
-        <Provider store={createStore(rootReducer, enhancer)}>
+        <Provider store={store}>
             {children}
         </Provider>
     );
 };
 
 ReduxProvider.propTypes = {
-    jahiaCtx: PropTypes.object.isRequired,
-
     children: PropTypes.element.isRequired
 };
 
