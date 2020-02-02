e1f055d8a26dbac834cca13f7db17c52ae441081
Thomas Draier <tdraier@jahia.com>
2020-02-02 20:45:43 +0100

    BACKLOG-12184 : Makes root components configurable

--- src/javascript/Jahia/Jahia.container.jsx
+++ src/javascript/Jahia/Jahia.container.jsx
@@ -13,46 +13,73 @@ import thunk from 'redux-thunk';
 
 const JahiaContainer = ({jahiaCtx}) => {
     const history = createBrowserHistory({basename: jahiaCtx.contextPath + jahiaCtx.urlbase});
+
+    // Register redux
+    registry.add('app', 'redux', {
+        targets: ['root:1'],
+        render: next => (<ReduxProvider jahiaCtx={jahiaCtx}>{next}</ReduxProvider>)
+    });
     registry.add('redux-middleware', 'batch', {middleware: batchDispatchMiddleware});
     registry.add('redux-middleware', 'thunk', {middleware: thunk});
+
+    // Connected router
+    registry.add('app', 'router', {
+        targets: ['root:2'],
+        render: next => <ConnectedRouter history={history}>{next}</ConnectedRouter>
+    });
     registry.add('redux-reducer', 'router', {reducer: connectRouter(history)});
     registry.add('redux-middleware', 'router', {middleware: routerMiddleware(history)});
 
-    return (
-        <ReduxProvider jahiaCtx={jahiaCtx}>
-            <ConnectedRouter history={history}>
-                <JahiaContext.Provider value={jahiaCtx}>
-                    <Jahia routes={registry.find({type: 'route', target: 'nav-root-top'})}
-                           topNavGroups={[
-                               <PrimaryNavGroup key="topNavGroup"
-                                                navItems={registry.find({
-                                                    type: 'topNavGroup',
-                                                    target: 'nav-root-top'
-                                                })}/>
-                           ]}
-                           bottomNavGroups={[
-                               <PrimaryNavGroup key="bottomDocumentationGroup"
-                                                isDisplayedWhenCollapsed={false}
-                                                navItems={registry.find({
-                                                    type: 'bottomDocumentationGroup',
-                                                    target: 'nav-root-bottom'
-                                                })}/>,
-                               <PrimaryNavGroup key="bottomProfileGroup"
-                                                navItems={registry.find({
-                                                    type: 'bottomProfileGroup',
-                                                    target: 'nav-root-bottom'
-                                                })}/>,
-                               <PrimaryNavGroup key="bottomAdminGroup"
-                                                navItems={registry.find({
-                                                    type: 'bottomAdminGroup',
-                                                    target: 'nav-root-bottom'
-                                                })}/>
-                           ]}
-                    />
-                </JahiaContext.Provider>
-            </ConnectedRouter>
-        </ReduxProvider>
+    // Jahia Context
+    registry.add('app', 'jahiacontext', {
+        targets: ['root:3'],
+        render: next => <JahiaContext.Provider value={jahiaCtx}>{next}</JahiaContext.Provider>
+    });
+
+    // UI-root
+    registry.add('app', 'jahia', {
+        targets: ['root:99'], render: next => (
+            <>
+                <Jahia routes={registry.find({type: 'route', target: 'nav-root-top'})}
+                       topNavGroups={[
+                           <PrimaryNavGroup key="topNavGroup"
+                                            navItems={registry.find({
+                                                type: 'topNavGroup',
+                                                target: 'nav-root-top'
+                                            })}/>
+                       ]}
+                       bottomNavGroups={[
+                           <PrimaryNavGroup key="bottomDocumentationGroup"
+                                            isDisplayedWhenCollapsed={false}
+                                            navItems={registry.find({
+                                                type: 'bottomDocumentationGroup',
+                                                target: 'nav-root-bottom'
+                                            })}/>,
+                           <PrimaryNavGroup key="bottomProfileGroup"
+                                            navItems={registry.find({
+                                                type: 'bottomProfileGroup',
+                                                target: 'nav-root-bottom'
+                                            })}/>,
+                           <PrimaryNavGroup key="bottomAdminGroup"
+                                            navItems={registry.find({
+                                                type: 'bottomAdminGroup',
+                                                target: 'nav-root-bottom'
+                                            })}/>
+                       ]}
+                />
+                {next}
+            </>
+        )
+    });
+
+    // App shell
+    const apps = registry.find({type: 'app', target: 'root'}).map(m => m.render);
+    const render = apps.reduceRight((prevFn, nextFn) =>
+        (...args) => nextFn(prevFn(...args)),
+    value => value
     );
+
+    return render();
 };
 
 JahiaContainer.propTypes = {
