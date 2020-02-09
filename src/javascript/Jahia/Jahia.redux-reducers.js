1241f398f12346611cd6719a1ff61aad49c2d3ef
Thomas Draier <tdraier@jahia.com>
2020-02-09 15:44:24 +0100

    BACKLOG-12335 : Moved UI language to parent app

--- src/javascript/Jahia/Jahia.redux-reducers.js
+++ src/javascript/Jahia/Jahia.redux-reducers.js
@@ -1,9 +1,11 @@
 import {createActions, handleAction} from 'redux-actions';
 
 export const jahiaReduxReducers = (registry, jahiaCtx) => {
-    const {setSite, setLanguage} = createActions('SET_SITE', 'SET_LANGUAGE');
+    const {setSite, setLanguage, setUiLanguage} = createActions('SET_SITE', 'SET_LANGUAGE', 'SET_UI_LANGUAGE');
+
     const siteReducer = handleAction(setSite, (state, action) => action.payload, 'systemsite');
     const languageReducer = handleAction(setLanguage, (state, action) => action.payload, jahiaCtx.uilang);
+    const uiLanguageReducer = handleAction(setUiLanguage, (state, action) => action.payload, jahiaCtx.uilang);
 
     registry.add('redux-reducer', 'site', {
         reducer: siteReducer,
@@ -16,6 +18,13 @@ export const jahiaReduxReducers = (registry, jahiaCtx) => {
         reducer: languageReducer,
         actions: {
             setLanguage
+        }
+    });
+
+    registry.add('redux-reducer', 'uilang', {
+        reducer: uiLanguageReducer,
+        actions: {
+            setUiLanguage
         }
     });
 };
