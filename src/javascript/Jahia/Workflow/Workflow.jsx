481f2faf4020885fa78291daab6ef80956369c48
cedmail <cmailleux@jahia.com>
2020-02-10 11:49:20 -0500

    BACKLOG-12220: Load workflow from GWT

--- src/javascript/Jahia/Workflow/Workflow.jsx
+++ src/javascript/Jahia/Workflow/Workflow.jsx
@@ -1,5 +1,40 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
-export default function () {
-    return <h2 style={{color: 'white'}}>Workflow</h2>;
-}
+let renderWorkflow = function () {
+    if (window.authoringApi !== undefined && window.authoringApi.openWorkflow !== undefined) {
+        console.log('rendering workflow');
+        clearInterval(checkAuthoringApi);
+        let elementById = document.getElementById('workflowComponent');
+        if (elementById !== null) {
+            elementById.lastChild.remove();
+            window.authoringApi.openWorkflow('workflowComponent');
+        }
+
+        return null;
+    }
+};
+
+const checkAuthoringApi = setInterval(() => {
+    return renderWorkflow();
+},
+100);
+
+const Workflow = () => {
+    useEffect(() => {
+        return renderWorkflow();
+    }, []);
+    return (
+        <div style={{
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            position: 'fixed'
+        }}
+             id="workflowComponent"
+        >
+            <h1>Loading Workflow</h1>
+        </div>
+    );
+};
+
+export default Workflow;
