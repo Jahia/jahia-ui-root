baa273f02bec46ee8000cd231ea8707f17e2dea2
Alex Karmanov <akarmanov@jahia.com>
2020-02-11 13:29:41 -0500

    BACKLOG-12309 Add Promise.all() to handle callbacks (#42)

--- src/javascript/JahiaApp.jsx
+++ src/javascript/JahiaApp.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Jahia from './Jahia';
+import JahiaError from './JahiaApp.error';
 import {registry} from '@jahia/ui-extender';
 
 function render() {
+    const container = document.getElementById(window.contextJsParameters.targetId);
     // Init register from callbacks
-    registry.find({type: 'callback', target: 'jahiaApp-init'}).forEach(entry => entry.callback());
-    ReactDOM.render(<Jahia jahiaCtx={window.contextJsParameters}/>, document.getElementById(window.contextJsParameters.targetId));
+    const promises = [];
+    registry.find({type: 'callback', target: 'jahiaApp-init'}).forEach(entry => promises.push(entry.callback()));
+    Promise.all(promises).then(() => {
+        ReactDOM.render(<Jahia jahiaCtx={window.contextJsParameters}/>, container);
+    }).catch(() => {
+        console.error('Encountered error during callback handling:', promises);
+        ReactDOM.render(<JahiaError operatingMode={window.contextJsParameters.operatingMode}/>, container);
+    });
 }
 
 export {render};
