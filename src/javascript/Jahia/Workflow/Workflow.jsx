f24e113a4a9c812af8dd8821f173aaa25618f020
Cedric Mailleux <cmailleux@jahia.com>
2020-02-11 12:43:59 -0500

    BACKLOG-12220: Load workflow from GWT (#41)
    * BACKLOG-12220: Load workflow from GWT
    
    * BACKLOG-12220: Avoid loading workflow in component is not to be rendered
    
    * BACKLOG-12220: avoid returning renderWorklow() from useEffect

--- src/javascript/Jahia/Workflow/Workflow.jsx
+++ src/javascript/Jahia/Workflow/Workflow.jsx
@@ -1,5 +1,41 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
-export default function () {
-    return <h2 style={{color: 'white'}}>Workflow</h2>;
-}
+let renderWorkflow = function () {
+    if (window.authoringApi !== undefined && window.authoringApi.openWorkflow !== undefined) {
+        clearInterval(checkAuthoringApi);
+        let elementById = document.getElementById('workflowComponent');
+        if (elementById !== null) {
+            elementById.lastChild.remove();
+            window.authoringApi.openWorkflow('workflowComponent');
+        }
+    }
+};
+
+let checkAuthoringApi;
+
+const Workflow = () => {
+    useEffect(() => {
+        if (window.authoringApi === undefined || window.authoringApi.openWorkflow === undefined) {
+            checkAuthoringApi = setInterval(() => {
+                renderWorkflow();
+            },
+            100);
+        }
+
+        renderWorkflow();
+    }, []);
+    return (
+        <div style={{
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            position: 'fixed'
+        }}
+             id="workflowComponent"
+        >
+            <h1>Loading Workflow</h1>
+        </div>
+    );
+};
+
+export default Workflow;
