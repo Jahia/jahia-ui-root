8e34d215d8ff6659c3038923d2cbec4fd5436603
alex <alexandrk_@hotmail.com>
2020-02-18 14:05:50 -0500

    BACKLOG-12362 Remove use effect

--- src/javascript/Jahia/PageComposer/PageComposer.jsx
+++ src/javascript/Jahia/PageComposer/PageComposer.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useContext, useState} from 'react';
 import Iframe from 'react-iframe';
 import JahiaContext from '../Jahia.context';
 import {useHistory, useLocation} from 'react-router-dom';
