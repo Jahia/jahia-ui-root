96c264b600a62e9f46d34daa3b9e085e25665cf6
alex <alexandrk_@hotmail.com>
2020-02-24 09:19:33 -0500

    BACKLOG-12219 Add workflow query and poll, display badge

--- src/javascript/Jahia/Workflow/Workflow.routes.jsx
+++ src/javascript/Jahia/Workflow/Workflow.routes.jsx
@@ -1,19 +1,41 @@
 import React from 'react';
 import {registry} from '@jahia/ui-extender';
 import {useHistory} from 'react-router-dom';
-import {PrimaryNavItem} from '@jahia/moonstone';
+import {PrimaryNavItem} from '@jahia/moonstone/dist/index.js';
+import {Badge} from '@jahia/moonstone/dist/index.js';
 import {useTranslation} from 'react-i18next';
+import {useQuery} from 'react-apollo';
 import WorkflowIcon from '@jahia/moonstone/dist/icons/Workflow';
 import Workflow from './Workflow';
-
+import {NumberOfWorkflowsQuery} from './Workflow.gql-querys';
 const PATH = '/workflow';
+
+const POLL_INTERVAL = 10000; // 10 seconds
 
 export const WorkflowGroup = () => {
     const {t} = useTranslation();
     const history = useHistory();
+    const {data, loading} = useQuery(NumberOfWorkflowsQuery, {
+        pollInterval: POLL_INTERVAL
+    });
+
+    if (loading) {
+        return null;
+    }
+
+    const badge = data.jcr.activeWorkflowTaskCountForUser > 0 ? (
+        <Badge
+        size="small"
+        type="round"
+        color="danger"
+        label={data.jcr.activeWorkflowTaskCountForUser}
+        style={{position: 'absolute', top: '0.1rem', right: '-0.1rem'}}
+    />
+    ) : null;
 
     return (
         <PrimaryNavItem
+            badge={badge}
             isSelected={history.location.pathname.startsWith(PATH)}
             icon={<WorkflowIcon/>}
             label={t('jahia-ui-root:primaryNavigation.workflow.label')}
