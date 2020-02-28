10ab03969045aa00ae699fd7e7c2d369e7044298
Alex Karmanov <akarmanov@jahia.com>
2020-02-28 11:01:24 -0500

    Backlog 12362 (#45) : Allow URL bar to reflect navigation in page composer and handle back/forward button navigation
    * BACKLOG-12362: Reflect page-composer iframe path in history object
    
    * BACKLOG-12362:Manage history popState to browse back in edit mode. Also sync the title between frame and page composer
    
    * BACKLOG-12362 Use replace only for react router
    
    * BACKLOG-12362 Remove use effect
    
    * BACKLOG-12362:Update to new msg format
    
    * BACKLOG-12362:Fix window listener event registration
    
    * BACKLOG-12450 : rewrite and redirect rules for edit and appshell (#47)
    
    * BACKLOG-12450 : fix rules (#48)
    
    *  BACKLOG-12450 : renaming (#49)
    
    * BACKLOG-12450 : fix rewriting urls (#50)
    
    * BACKLOG-12362:rebasing to redirect=false
    
    * BACKLOG-12362: use redux state for site and language
    
    Co-authored-by: Cedric Mailleux <cmailleux@jahia.com>
    Co-authored-by: Yousria <ybenchadi@jahia.com>

--- src/javascript/Jahia/PageComposer/PageComposer.jsx
+++ src/javascript/Jahia/PageComposer/PageComposer.jsx
@@ -1,20 +1,84 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import Iframe from 'react-iframe';
 import {useSelector} from 'react-redux';
 import JahiaContext from '../Jahia.context';
+import {useHistory, useLocation} from 'react-router-dom';
 
-let path = (locale, siteKey) => {
-    return `/cms/edit/default/${locale}/sites/${siteKey}/home.html?redirect=false`;
+let path = (locale, siteKey, mainResourcePath) => {
+    return `/cms/edit/default/${locale}/sites/${siteKey}${mainResourcePath}?redirect=false`;
+};
+
+let initialValue = function (location, siteKey) {
+    let mainResourcePath = '/home.html';
+    if (!location.pathname.endsWith('page-composer') && location.pathname.indexOf(siteKey) >= 0) {
+        if (window.frames['page-composer-frame'] === undefined) {
+            mainResourcePath = location.pathname.split(siteKey)[1];
+        }
+    }
+
+    return mainResourcePath;
+};
+
+let history = null;
+
+let getPathFromChildIFrame = function () {
+    let framepathname = window.frames[1].location.pathname;
+
+    return framepathname.substr(framepathname.lastIndexOf('/sites/'));
+};
+
+const iFrameOnHistoryMessage = event => {
+    if (history !== null) {
+        if (event.origin !== window.location.origin) {
+            return;
+        }
+
+        if (event.data !== null && event.data.msg !== null) {
+            console.log('iFrameOnHistoryMessage', event.data);
+            if (event.data.msg === 'edit frame history updated') {
+                let pathFromChildIFrame = getPathFromChildIFrame();
+                let newPath = history.location.pathname.replace(/page-composer.*/gi, 'page-composer' + pathFromChildIFrame);
+                history.replace(newPath);
+            } else if (event.data.msg === 'setTitle') {
+                document.title = event.data.title;
+            }
+        }
+    }
 };
 
 export default function () {
     const jahiaContext = useContext(JahiaContext);
+    const composerLocation = useLocation();
+    history = useHistory();
     const current = useSelector(state => ({language: state.language, site: state.site}));
+    const [mainResourcePath] = useState(initialValue(composerLocation, current.site));
+    useEffect(() => {
+        if (window.frames['page-composer-frame'] !== undefined) {
+            window.addEventListener('message', iFrameOnHistoryMessage, false);
+        }
+
+        return () => {
+            window.removeEventListener('message', iFrameOnHistoryMessage, false);
+        };
+    });
+
+    const iFrameOnLoad = () => {
+        if (window.frames['page-composer-frame'] !== undefined) {
+            window.addEventListener('message', iFrameOnHistoryMessage, false);
+        }
+    };
 
     // Temporary solution
     if (current.site === 'systemsite') {
         return <h2 style={{color: 'white'}}>You need to create a site to see this page</h2>;
     }
 
-    return <Iframe url={jahiaContext.contextPath + path(current.language, current.site)} width="100%" height="100%"/>;
+    return (
+        <Iframe url={jahiaContext.contextPath + path(current.language, current.site, mainResourcePath)}
+                width="100%"
+                height="100%"
+                id="page-composer-frame"
+                onLoad={iFrameOnLoad}
+        />
+    );
 }
