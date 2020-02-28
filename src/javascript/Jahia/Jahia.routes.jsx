713937256659ce3e3ea65efc215a321f43c8ea28
Thomas Draier <tdraier@jahia.com>
2020-02-28 15:15:25 +0100

    BACKLOG-12161 : Handle links transformation

--- src/javascript/Jahia/Jahia.routes.jsx
+++ src/javascript/Jahia/Jahia.routes.jsx
@@ -1,18 +1,44 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
+import {useSiteInfo} from '@jahia/data-helper';
 import {registry} from '@jahia/ui-extender';
 import {Button, PrimaryNavItem} from '@jahia/moonstone';
 import {useTranslation} from 'react-i18next';
 import Person from '@jahia/moonstone/dist/icons/Person';
 import Power from '@jahia/moonstone/dist/icons/Power';
 
+const uiLangRegexp = /\$ui-lang\(\[([-a-zA-Z,]+)\],([-a-zA-Z]+)\)/g
+
+const transformLink = (link, siteInfo, displayLanguage) => {
+    link = link.replace('$site-servername', siteInfo.serverName);
+    link = link.replace('$dx-version', window.contextJsParameters.version);
+
+    let match = uiLangRegexp.exec(link);
+    while (match !== null) {
+        if (match[1].split(',').includes(displayLanguage)) {
+            link = link.replace(match[0], displayLanguage);
+        } else {
+            link = link.replace(match[0], match[2]);
+        }
+
+        match = uiLangRegexp.exec(link);
+    }
+    return link;
+};
+
 // TODO separate into different folders
 export const DocumentationGroup = () => {
     const {t} = useTranslation();
+    const {siteKey, displayLanguage} = useSelector(state => ({displayLanguage: state.uilang, siteKey: state.site}));
+    const {siteInfo} = useSiteInfo({siteKey, displayLanguage});
+
+    let links = window.contextJsParameters.config.links;
+
     return (
         <>
-            <PrimaryNavItem url={window.contextJsParameters.links.whatsNew}
+            <PrimaryNavItem url={transformLink(links.whatsNew, siteInfo, displayLanguage)}
                             label={t('jahia-ui-root:primaryNavigation.documentation.newAtJahia')}/>
-            <PrimaryNavItem url={window.contextJsParameters.links.documentation}
+            <PrimaryNavItem url={transformLink(links.documentation, siteInfo, displayLanguage)}
                             label={t('jahia-ui-root:primaryNavigation.documentation.label')}/>
         </>
     );
