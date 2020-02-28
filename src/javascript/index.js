f67436a2f19ce23922dc33bcce648eccac77eed8
Thomas Draier <tdraier@jahia.com>
2020-02-28 16:31:39 +0100

    BACKLOG-12161 : Add TODO

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,5 +1,6 @@
 import {registry} from '@jahia/ui-extender';
 
+// TODO : registration should be done synchronously so that they are called in callback order
 registry.add('callback', 'jahiaUiRoot', {
     targets: ['jahiaApp-init:99'],
     callback: () => import('./JahiaApp.register')
