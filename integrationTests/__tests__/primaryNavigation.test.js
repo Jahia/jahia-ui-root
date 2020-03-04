4a0514d357b09537504b410211b1dfc637898e8d
astritademi <aademi@jahia.com>
2020-03-04 11:52:13 -0500

    BACKLOG-12521 created primary and secondary nav tests

--- integrationTests/__tests__/primaryNavigation.test.js
+++ integrationTests/__tests__/primaryNavigation.test.js
@@ -8,6 +8,7 @@ describe('primary nav bar tests', () => {
     beforeAll(async () => {
         page = await global.__BROWSER__.newPage();
         await page.goto(process.env[constants.TEST_URL] + '/start', {waitUntil: 'networkidle0'});
+        // Login
         await page.type('input[name=\'username\']', process.env[constants.JAHIA_USERNAME], {delay: 50});
         await page.type('input[name=\'password\']', process.env[constants.JAHIA_PASSWORD], {delay: 50});
         await Promise.all([
@@ -38,65 +39,81 @@ describe('primary nav bar tests', () => {
         expect(await navEl.screenshot()).toMatchImageSnapshot();
     });
 
-    // It('collapses the nav bar', async () => {
-    //     await page.goto(process.env[constants.TEST_URL] + '/jahia', {waitUntil: 'networkidle2'});
-    //
-    //     await page.click('nav button');
-    //     // Wait until nav bar is fully expanded
-    //     await page.waitForFunction('document.querySelector(\'nav\').offsetWidth == 300');
-    //
-    //     await page.click('nav button');
-    //     // Wait until nav bar is fully collapsed
-    //     await page.waitForFunction('document.querySelector(\'nav\').offsetWidth == 56');
-    //
-    //     // Remove all children nodes from nav
-    //     await page.evaluate(() => {
-    //         let e = document.querySelector('nav');
-    //         let child = e.lastElementChild;
-    //         while (child) {
-    //             e.removeChild(child);
-    //             child = e.lastElementChild;
-    //         }
-    //     });
-    //
-    //     const navEl = await page.$('nav');
-    //
-    //     expect(await navEl.screenshot()).toMatchImageSnapshot();
-    // });
-    //
-    // it('checks header renders correctly', async () => {
-    //     await page.goto(process.env[constants.TEST_URL] + '/jahia', {waitUntil: 'networkidle2'});
-    //
-    //     await page.click('nav button');
-    //     // Wait until nav bar is fully expanded
-    //     await page.waitForFunction('document.querySelector(\'nav\').offsetWidth == 300');
-    //
-    //     const navEl = await page.$('nav div');
-    //
-    //     expect(await navEl.screenshot()).toMatchImageSnapshot();
-    // });
-    //
-    // it('checks that the top-half renders correctly', async () => {
-    //     await page.goto(process.env[constants.TEST_URL] + '/jahia', {waitUntil: 'networkidle2'});
-    //
-    //     await page.click('nav button');
-    //     // Wait until nav bar is fully expanded
-    //     await page.waitForFunction('document.querySelector(\'nav\').offsetWidth == 300');
-    //
-    //     const navEl = await page.$$('nav ul');
-    //
-    //     expect(await navEl[0].screenshot()).toMatchImageSnapshot();
-    // });
-    //
-    // it('checks that the bottom-half renders correctly', async () => {
-    //     await page.goto(process.env[constants.TEST_URL] + '/jahia', {waitUntil: 'networkidle2'});
-    //
-    //     await page.click('nav button');
-    //     // Wait until nav bar is fully expanded
-    //     await page.waitForFunction('document.querySelector(\'nav\').offsetWidth == 300');
-    //
-    //     const navEl = await page.$$('nav ul');
-    //
-    //     expect(await navEl[3].screenshot()).toMatchImageSnapshot();
-    // });
+    it('collapses the nav bar', async () => {
+        await page.goto(process.env[constants.TEST_URL] + '/jahia', {waitUntil: 'networkidle2'});
+
+        await page.click('nav button');
+        // Wait until nav bar is fully expanded
+        await page.waitForFunction('document.querySelector(\'nav\').offsetWidth == 300');
+
+        await page.click('nav button');
+        // Wait until nav bar is fully collapsed
+        await page.waitForFunction('document.querySelector(\'nav\').offsetWidth == 56');
+
+        // Remove all children nodes from nav
+        await page.evaluate(() => {
+            let e = document.querySelector('nav');
+            let child = e.lastElementChild;
+            while (child) {
+                e.removeChild(child);
+                child = e.lastElementChild;
+            }
+        });
+
+        const navEl = await page.$('nav');
+
+        expect(await navEl.screenshot()).toMatchImageSnapshot();
+    });
+
+    it('checks header renders correctly', async () => {
+        await page.goto(process.env[constants.TEST_URL] + '/jahia', {waitUntil: 'networkidle2'});
+
+        await page.click('nav button');
+        // Wait until nav bar is fully expanded
+        await page.waitForFunction('document.querySelector(\'nav\').offsetWidth == 300');
+
+        const navEl = await page.$('nav div');
+
+        expect(await navEl.screenshot()).toMatchImageSnapshot();
+    });
+
+    it('checks that the top-half renders correctly', async () => {
+        await page.goto(process.env[constants.TEST_URL] + '/jahia', {waitUntil: 'networkidle2'});
+
+        await page.click('nav button');
+        // Wait until nav bar is fully expanded
+        await page.waitForFunction('document.querySelector(\'nav\').offsetWidth == 300');
+
+        const navEl = await page.$$('nav ul');
+
+        expect(await navEl[0].screenshot()).toMatchImageSnapshot();
+    });
+
+    it('checks that the bottom-half renders correctly', async () => {
+        await page.goto(process.env[constants.TEST_URL] + '/jahia', {waitUntil: 'networkidle2'});
+
+        await page.click('nav button');
+        // Wait until nav bar is fully expanded
+        await page.waitForFunction('document.querySelector(\'nav\').offsetWidth == 300');
+
+        const navEl = await page.$$('nav ul');
+
+        expect(await navEl[3].screenshot()).toMatchImageSnapshot();
+    });
+
+    afterAll(async () => {
+        // Logout
+        await page.goto(process.env[constants.TEST_URL] + '/jahia', {waitUntil: 'networkidle2'});
+        await page.click('nav button');
+        // Wait until nav bar is fully expanded
+        await page.waitForFunction('document.querySelector(\'nav\').offsetWidth == 300');
+        await Promise.all([
+            page.waitForNavigation(),
+            page.click('nav ul button')
+        ]);
+        // Check that we are redirected to /start page after logout
+        expect(await page.url()).toBe(process.env[constants.TEST_URL] + '/start');
+
+        await page.close();
+    });
 }, timeout);
