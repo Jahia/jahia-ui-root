c2186e82bbb10812fc153226073e0ecde2a30fd8
Francois G <f.gerthoffert@gmail.com>
2020-03-04 16:16:30 -0500

    Revert "BACKLOG-12521 created primary and secondary nav tests (#72)"
    This reverts commit 7def6948f0e5cd502d36d0906cfe724d816985c8.

--- integrationTests/setup.js
+++ integrationTests/setup.js
@@ -10,7 +10,7 @@ const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup');
 module.exports = async function () {
     const browser = await puppeteer.launch(
         {
-            SlowMo: 100,
+            // SlowMo: 50,
             headless: true,
             args: ['--start-fullscreen', '-–no-sandbox', '-–disable-setuid-sandbox']
             // Timeout: 100000
