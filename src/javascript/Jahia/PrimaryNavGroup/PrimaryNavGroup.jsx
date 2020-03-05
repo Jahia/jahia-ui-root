e399d84e4fdeea883fc0000ff5f10ae9e00d89de
dgaillard <dgaillard@jahia.com>
2020-03-05 11:11:13 +0100

    BACKLOG-12230 allow to render menu item without render function (#73)

--- src/javascript/Jahia/PrimaryNavGroup/PrimaryNavGroup.jsx
+++ src/javascript/Jahia/PrimaryNavGroup/PrimaryNavGroup.jsx
@@ -1,15 +1,35 @@
 import React, {Suspense} from 'react';
 import PropTypes from 'prop-types';
-import {PrimaryNavItemsGroup} from '@jahia/moonstone';
+import {PrimaryNavItem, PrimaryNavItemsGroup} from '@jahia/moonstone';
 import {registry} from '@jahia/ui-extender';
+import {useTranslation} from 'react-i18next';
+import {useHistory} from 'react-router';
 
-export const PrimaryNavGroup = ({isDisplayedWhenCollapsed, target}) => (
-    <Suspense fallback="loading...">
-        <PrimaryNavItemsGroup isDisplayedWhenCollapsed={isDisplayedWhenCollapsed}>
-            {registry.find({type: 'primary-nav-item', target}).map(item => React.cloneElement(item.render(), {key: item.key}))}
-        </PrimaryNavItemsGroup>
-    </Suspense>
-);
+export const PrimaryNavGroup = ({isDisplayedWhenCollapsed, target}) => {
+    const {t} = useTranslation();
+    const history = useHistory();
+
+    return (
+        <Suspense fallback="loading...">
+            <PrimaryNavItemsGroup isDisplayedWhenCollapsed={isDisplayedWhenCollapsed}>
+                {registry.find({type: 'primary-nav-item', target}).map(item => {
+                    if (item.render) {
+                        return React.cloneElement(item.render(), {key: item.key});
+                    }
+
+                    return (
+                        <PrimaryNavItem key={item.key}
+                                        role={`${item.key}-menu-item`}
+                                        isSelected={history.location.pathname.startsWith(item.path)}
+                                        icon={item.icon}
+                                        label={t(item.label)}
+                                        onClick={() => history.push(item.path)}/>
+                    );
+                })}
+            </PrimaryNavItemsGroup>
+        </Suspense>
+    );
+};
 
 PrimaryNavGroup.defaultProps = {
     isDisplayedWhenCollapsed: true
