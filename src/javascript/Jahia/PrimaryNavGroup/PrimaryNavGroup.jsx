ea9d624bc017190e1f9d755fbea28c524f3792e4
alex <alexandrk_@hotmail.com>
2020-03-16 09:45:54 -0400

    BACKLOG-12752 Extract filtering of nav items into a hook

--- src/javascript/Jahia/PrimaryNavGroup/PrimaryNavGroup.jsx
+++ src/javascript/Jahia/PrimaryNavGroup/PrimaryNavGroup.jsx
@@ -1,4 +1,4 @@
-import React, {Suspense} from 'react';
+import React, {Suspense, useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import {PrimaryNavItem, PrimaryNavItemsGroup} from '@jahia/moonstone';
 import {registry} from '@jahia/ui-extender';
@@ -7,43 +7,63 @@ import {useHistory} from 'react-router';
 import {useNodeInfo} from '@jahia/data-helper';
 import {useSelector} from 'react-redux';
 
+const usePermissionFilter = (navItems, site, language) => {
+    const [requiredPathsAndPermissions, setRequiredPathsAndPermissions] = useState({
+        requiredPermission: ['jcr:read_default'],
+        requiredPaths: []
+    });
+    const permissions = useNodeInfo({paths: requiredPathsAndPermissions.requiredPaths, language: language},
+        {getPermissions: requiredPathsAndPermissions.requiredPermission});
+
+    useEffect(() => {
+        const requiredPermission = requiredPathsAndPermissions.requiredPermission;
+        const requiredPaths = requiredPathsAndPermissions.requiredPaths;
+
+        navItems.filter(navItem => navItem.requiredPermission !== undefined).forEach(navItem => {
+            if (!requiredPermission.includes(navItem.requiredPermission)) {
+                requiredPermission.push(navItem.requiredPermission);
+            }
+
+            if (navItem.requiredPermissionPath !== undefined && !requiredPaths.includes(navItem.requiredPermissionPath)) {
+                requiredPaths.push(navItem.requiredPermissionPath);
+            }
+        });
+        if (requiredPaths.length === 0) {
+            requiredPaths.push('/sites/' + site);
+        }
+
+        setRequiredPathsAndPermissions({
+            requiredPermission: requiredPermission,
+            requiredPaths: requiredPaths
+        });
+        // eslint-disable-next-line
+    }, [navItems.length, site, language]);
+
+    if (permissions.loading === true || permissions.nodes === null) {
+        return [];
+    }
+
+    return navItems.filter(navItem => navItem.requiredPermission === undefined ||
+        permissions.nodes.find(node => {
+            if (navItem.requiredPermissionPath !== undefined) {
+                return node.path === navItem.requiredPermissionPath;
+            }
+
+            return node.path === '/sites/' + site;
+        })[navItem.requiredPermission]);
+};
+
 export const PrimaryNavGroup = ({isDisplayedWhenCollapsed, target}) => {
     const {t} = useTranslation();
     const history = useHistory();
     const current = useSelector(state => ({language: state.language, site: state.site}));
     const navItems = registry.find({type: 'primary-nav-item', target});
-    const requiredPermission = ['jcr:read_default'];
-    const requiredPaths = [];
-    navItems.filter(navItem => navItem.requiredPermission !== undefined).forEach(navItem => {
-        if (!requiredPermission.includes(navItem.requiredPermission)) {
-            requiredPermission.push(navItem.requiredPermission);
-        }
-
-        if (navItem.requiredPermissionPath !== undefined && !requiredPaths.includes(navItem.requiredPermissionPath)) {
-            requiredPaths.push(navItem.requiredPermissionPath);
-        }
-    });
-    if (requiredPaths.length === 0) {
-        requiredPaths.push('/sites/' + current.site);
-    }
-
-    const permissions = useNodeInfo({paths: requiredPaths, language: 'en'},
-        {getPermissions: requiredPermission});
-    if (permissions.loading === true || permissions.nodes === null) {
-        return null;
-    }
+    const filteredNavItems = usePermissionFilter(navItems, current.site, current.language);
 
     return (
         <Suspense fallback="loading...">
             <PrimaryNavItemsGroup isDisplayedWhenCollapsed={isDisplayedWhenCollapsed}>
-                {navItems.filter(navItem => navItem.requiredPermission === undefined ||
-                    permissions.nodes.find(node => {
-                        if (navItem.requiredPermissionPath !== undefined) {
-                            return node.path === navItem.requiredPermissionPath;
-                        }
-
-                        return node.path === '/sites/' + current.site;
-                    })[navItem.requiredPermission]).map(item => {
+                {filteredNavItems.map(item => {
                     if (item.render) {
                         return React.cloneElement(item.render(), {key: item.key});
                     }
