47cf0f4d9fa0d81a7769d50c096ee13146b3a4ce
Thomas Draier <tdraier@jahia.com>
2020-04-30 22:22:29 +0200

    BACKLOG-13556: Add session expiration check (#102)

---
+++ src/javascript/Jahia/LoginCheck/LoginCheck.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {useQuery} from '@apollo/react-hooks';
+import {GetUserQuery} from './LoginCheck.gql';
+import styles from './LoginCheck.scss';
+import {Typography, Button} from '@jahia/moonstone';
+import {useTranslation} from 'react-i18next';
+
+export const LoginCheck = () => {
+    const {t} = useTranslation('jahia-ui-root');
+    const res = useQuery(GetUserQuery, {
+        pollInterval: 10000
+    });
+
+    const sessionInvalid = Boolean(res.data && res.data.currentUser.name === 'guest');
+
+    return sessionInvalid && (
+        <div className={styles.root}>
+            <div className={styles.backdrop}/>
+            <div className={styles.modal}>
+                <Typography variant="heading">{t('sessionExpiration.title')}</Typography>
+                <Typography>
+                    {t('sessionExpiration.text')}
+                    <br/>
+                    {t('sessionExpiration.text2')}
+                </Typography>
+                <Button className={styles.button}
+                        color="accent"
+                        size="big"
+                        label={t('sessionExpiration.button')}
+                        onClick={() => {
+                            location.reload();
+                        }}
+                />
+            </div>
+        </div>
+    );
+};
