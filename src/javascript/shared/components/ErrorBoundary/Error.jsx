26b4a27a0e8e9ea7dcc8f16f837d5ea3c32b87c3
jsinovassin <58434978+jsinovassin@users.noreply.github.com>
2022-06-15 13:55:37 +0200

    QA-14158: add back button on error page (#144)

--- src/javascript/shared/components/ErrorBoundary/Error.jsx
+++ src/javascript/shared/components/ErrorBoundary/Error.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Button, Reload} from '@jahia/moonstone';
+import {ArrowLeft, Button, Reload} from '@jahia/moonstone';
 import PropTypes from 'prop-types';
 import {useTranslation} from 'react-i18next';
 import styles from './Error.scss';
 import {ErrorLayout} from './ErrorLayout';
 
-export const Error = ({error, reset, ...props}) => {
+export const Error = ({error, reset, goBack, ...props}) => {
     const {t} = useTranslation('jahia-ui-root');
     return (
         <ErrorLayout title={t('error.error500.label')}
@@ -13,7 +13,20 @@ export const Error = ({error, reset, ...props}) => {
                      className={styles.page500}
                      footer={(
                          <>
-                             <Button icon={<Reload/>} size="big" label={t('error.error500.retry')} color="accent" className={styles['pageError-button']} onClick={reset}/>
+                             <div className={styles['pageError-buttons']}>
+                                 <Button icon={<Reload/>}
+                                         size="big"
+                                         label={t('error.error500.retry')}
+                                         color="accent"
+                                         className={styles['pageError-button']}
+                                         onClick={reset}/>
+                                 <Button icon={<ArrowLeft/>}
+                                         size="big"
+                                         label={t('error.back')}
+                                         color="default"
+                                         className={styles['pageError-button']}
+                                         onClick={goBack}/>
+                             </div>
                              <pre className={styles['pageError-log']}>
                                  {error.stack}
                              </pre>
@@ -26,5 +39,6 @@ export const Error = ({error, reset, ...props}) => {
 
 Error.propTypes = {
     error: PropTypes.string,
-    reset: PropTypes.func
+    reset: PropTypes.func,
+    goBack: PropTypes.func
 };
