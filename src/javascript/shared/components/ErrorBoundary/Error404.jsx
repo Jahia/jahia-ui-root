813bbf9f91d87fefd536fe6dc4e61f41b78bf56a
Cedric Mailleux <cmailleux@jahia.com>
2023-06-22 11:13:53 -0400

    BACKLOG-20518: Allow to pass a label for title for 404 error page

--- src/javascript/shared/components/ErrorBoundary/Error404.jsx
+++ src/javascript/shared/components/ErrorBoundary/Error404.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import styles from './Error.scss';
 import {ErrorLayout} from './ErrorLayout';
 import {useTranslation} from 'react-i18next';
+import PropTypes from 'prop-types';
 
 export const Error404 = props => {
     const {t} = useTranslation('jahia-ui-root');
     return (
-        <ErrorLayout title={t('error.error404.label')}
+        <ErrorLayout title={props.label === undefined ? t('error.error404.label') : props.label}
                      subtitles={[t('error.error404.subtitle1'), t('error.error404.subtitle2')]}
                      className={styles.page404}
                      {...props}
@@ -15,5 +16,6 @@ export const Error404 = props => {
 };
 
 Error404.propTypes = {
+    label: PropTypes.string
 };
 
